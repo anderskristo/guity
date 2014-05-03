f1137e68fa16e6620a8a7ff24a17bbe51a0dfb73
anderskristo <anderskristo@gmail.com>
2014-05-03 14:17:22 +0200

    newrelic

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
+var newrelic = require('newrelic');
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 var logger = require('morgan');
- 
+
 app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
